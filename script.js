490a8d4e0889646bca0e1b4b4a09df095d2eb2dd
brenno <brenno.amaral.maques@gmail.com>
2021-02-04 06:02:29 -0300

    atualização icone
    Ícone estilizado e botão do icone formatado para o tamanho exato do ícone

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const menu = document.querySelector('div#menu');
 const btnMenu = document.querySelector('button#btnMenu');
-const iconMenu = btnMenu.firstChild;
+const iconMenu = document.querySelector('img#iconMenu');
 let trueOrFalse = false;
 
 btnMenu.addEventListener('click', ()=>{
     if(trueOrFalse == false){
         menu.classList.add('open');
         btnMenu.classList.add('open');
+        iconMenu.src = 'img/icon-menu-white.svg';
         trueOrFalse = true;
     }else{
         menu.classList.remove('open');
         btnMenu.classList.remove('open');
+        iconMenu.src = 'img/icon-menu.svg';
         trueOrFalse = false;
     }
 });
