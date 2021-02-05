4e6bd02c552c3c355d46ee40a42e3297b2d9f93d
brenno <brenno.amaral.maques@gmail.com>
2021-02-05 02:13:51 -0300

    responsividade e icone menu
    Acrescentei mediaqueries para melhorar para dar inicio a responsividade do site; Adicinei o icone do menu na cor branca para ficaraparente quando a barra lateral é deslocada.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('div#menu');
 const btnMenu = document.querySelector('button#btnMenu');
 const iconMenu = document.querySelector('img#iconMenu');
+const icon = document.querySelector('img#icon');
 let trueOrFalse = false;
 
 btnMenu.addEventListener('click', ()=>{
@@ -8,6 +9,7 @@ btnMenu.addEventListener('click', ()=>{
         menu.classList.add('open');
         btnMenu.classList.add('open');
         iconMenu.src = 'img/icon-menu-white.svg';
+        icon.src= '';
         trueOrFalse = true;
     }else{
         menu.classList.remove('open');
