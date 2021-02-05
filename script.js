25af88b6a30cd3ad606faef9d720952943817a7e
brenno <brenno.amaral.maques@gmail.com>
2021-02-05 06:08:50 -0300

    update de melhoria versão Desktop
    Melhorando o design da nav bar da versão desktop e retirando: função de Screen width, icon(desktop version)

--- script.js
+++ script.js
@@ -4,31 +4,20 @@ const iconMenu = document.querySelector('img#iconMenu');
 const icon = document.querySelector('img#icon');
 let trueOrFalse = false;
 
-window.onresize= function(){
-    let screenWidth = window.innerWidth;
-    if(Number(screenWidth<=600)){
-        icon.src = 'none';
-    }else{
-        icon.src= 'img/icon.png';
-    }
-}
+
 
 btnMenu.addEventListener('click', ()=>{
     
-    
-    
+
     if(trueOrFalse == false){
         menu.classList.add('open');
         btnMenu.classList.add('open');
         iconMenu.src = 'img/icon-menu-white.svg';
-        let screnWidth = window.innerWidth;
-        
         trueOrFalse = true;
     }else{
         menu.classList.remove('open');
         btnMenu.classList.remove('open');
         iconMenu.src = 'img/icon-menu.svg';
-        
         trueOrFalse = false;
     }
 
